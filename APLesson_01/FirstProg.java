2692b2902e7f24c4f028ebd2e0e26e3cf24fe861
John-C-Calhoun <McMahonM4859@sduhsd.lan>
2016-09-07 14:15:10 -0700

    Period 7 Michael McMahon Asciart
    This is my asciart. There are many like it but this one is mine.

---
+++ APLesson_01/FirstProg.java
@@ -0,0 +1,17 @@
+public class FirstProg
+{
+	public static void main(String[]args)
+	{
+		System.out.println("MMMM           MMMM      IIIIII     KKKKK    KKKKK       EEEEEEEEEEE");
+		System.out.println("MMMMM         MMMMM      IIIIII     KKKKK   KKKKK        EEEEEEEEEEE");
+		System.out.println("MMMMMM       MMMMMM      IIIIII     KKKKK  KKKKK         EEEE");
+		System.out.println("MMMMMMM     MMMMMMM      IIIIII     KKKKK KKKKK          EEEE");
+		System.out.println("MMMMMMMM   MMMMMMMM      IIIIII     KKKKKKKKKK           EEEEEEEE");
+		System.out.println("MMMMMMMMM MMMMMMMMM      IIIIII     KKKKKKKKK            EEEEEEEE");
+		System.out.println("MMM  MMMMMMMMM  MMM      IIIIII     KKKKKKKKKK           EEEEEEEE");
+		System.out.println("MMM    MMMMM    MMM      IIIIII     KKKKK KKKKKK         EEEE");
+		System.out.println("MMM             MMM      IIIIII     KKKKK  KKKKKK        EEEE");
+		System.out.println("MMM             MMM      IIIIII     KKKKK   KKKKKK       EEEEEEEEEEE");
+		System.out.println("MMM             MMM      IIIIII     KKKKK     KKKKKK     EEEEEEEEEEE");
+	}
+}
---
+++ APLesson_01/SecondArt.java
@@ -0,0 +1,15 @@
+public class SecondArt
+{
+	public static void main(String[]args)
+	{
+		System.out.println("  _");
+		System.out.println(" ( \\");
+		System.out.println("  ) )   _...._ ");
+		System.out.println(" ( (   .      .  A.-.A");
+		System.out.println("  \\  /         \\/ , , \\");
+		System.out.println("   \\           ='  T  I");
+		System.out.println("      I  I     -    ,");
+		System.out.println("     / //      I  II");
+		System.out.println("    /_,))      I_, ))");
+	}
+}
