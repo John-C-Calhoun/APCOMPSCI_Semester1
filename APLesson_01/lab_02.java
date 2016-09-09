094f0f569abf50c5ea16ab1a26e00f9ffd64a932
John-C-Calhoun <michael.mcmahon99@hotmail.com>
2016-09-09 13:47:03 -0700

    Lab_02

---
+++ APLesson_01/lab_02.java
@@ -0,0 +1,27 @@
+public class lab_02
+{
+	public static void main(String[]args)
+	{
+		int x = 14;
+		int y = 68;
+		int multprod = x * y;
+		
+		System.out.println("14 multiplied by 68 is " + multprod);
+		
+		int height = 5;
+		int length = 4;
+		int width = 6;
+		
+		System.out.println("The surface area of your rectangle is " + length * width * height);
+		
+		String first = "Michael McMahon";
+		String second = "3710 Del Mar Heights Rd";
+		String third = "San Diego";
+		String fourth = " 92130";
+		
+		System.out.println(first);
+		System.out.println(second);
+		System.out.println(third);
+		System.out.println(fourth);
+	}
+}
