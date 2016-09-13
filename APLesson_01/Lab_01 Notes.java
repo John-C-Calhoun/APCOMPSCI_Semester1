87a0c89ad1396dfded4e845b0deb45aad32fb122
John-C-Calhoun <michael.mcmahon99@hotmail.com>
2016-09-13 14:06:35 -0700

    Lab_01 ascii art and notes

---
+++ APLesson_01/Lab_01 Notes.java
@@ -0,0 +1,11 @@
+public class Lab_01 Notes
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Use Quotations");
+		System.out.println("End each line with a semicolon");
+		System.out.println("Compiling and executing are F6");
+		System.out.println("\" can be used to add quotations in a line of text after execution");
+		
+	}
+}
