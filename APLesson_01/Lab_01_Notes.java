854209bc5df42fa565630a999c4c39800438b9e6
John-C-Calhoun <michael.mcmahon99@hotmail.com>
2016-09-13 14:24:59 -0700

    Lab_01 Notes and Ascii Art

---
+++ APLesson_01/Lab_01_Notes.java
@@ -0,0 +1,17 @@
+public class Lab_01_Notes
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Use Quotations");
+		System.out.println("End each line with a semicolon.");
+		System.out.println("Compiling and executing are F6.");
+		System.out.println("Use a space in quotations before inserting int or string to make the sentence spacing look correct.");
+		System.out.println("Forward slash n can be used to make text go down one line.");
+		System.out.println("Foward slash t can be used to add a tab space to text.");
+		System.out.println("The title of a java file cannot have spaces.");
+		System.out.println("Two forward slashes will become a single slash after compiling.");
+		System.out.println("Spaces take up less space then a letter after compiling, so beware.");
+		System.out.println("ln and int must be used appropriately.");
+		System.out.println("Abackslash with a quotation can be used to add quotations in text.");
+	}
+}
