674432f1a495e41501f0542b7cf03c04fc57a5ca
John-C-Calhoun <michael.mcmahon99@hotmail.com>
2016-11-28 14:41:56 -0800

    APLesson_08

---
+++ APLesson_08/Underscores.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Underscores
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new canner(System.in)
+		System.out.println("Please enter a sentence: ");
+		String input = kb.nextLine();
+		replace(input, 0);
+	}
+	
+	public static String replace(String word)
+	{
+		if ()
+			return System.out.println(word);
+		else
+		{
+			System.out.ptintln(Sentence.substring(0, word.indexOf(""));
+			replace()
+		}
+	}
+}
