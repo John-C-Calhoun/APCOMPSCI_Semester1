ce15a268d92da9090dc0e7be68ac94b6127669da
John-C-Calhoun <michael.mcmahon99@hotmail.com>
2016-11-28 20:57:51 -0800

    Revert "APLesson_08"
    This reverts commit 674432f1a495e41501f0542b7cf03c04fc57a5ca.

--- APLesson_07/ReplaceAts.java
+++ APLesson_07/ReplaceAts.java
@@ -6,17 +6,17 @@ public class ReplaceAts
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Please enter a sentence: ");
+		System.out.println("Please enter a number: ");
 		sentence = kb.nextLine();
 		
 		replace();
+		System.out.println();
 	}
 	public static void replace()
 	{
 		while(sentence.indexOf("a") >= 0)
 		{
-			sentence = sentence.substring(0, sentence.indexOf("a")) + "@" + sentence.substring(sentence.indexOf("a") + 1);
+			sentence = sentence.substring(0, sentence.indexOf("a")) + sentence.substring(sentence.indexOf("a") + 1);
 		}
-		System.out.println("With all a's replaced with @'s... " + sentence);
 	}
 }
