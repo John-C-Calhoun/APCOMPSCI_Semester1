8c8ac7318be5323c3bd7ba472ec3201fb028c23c
John-C-Calhoun <michael.mcmahon99@hotmail.com>
2017-01-06 14:23:09 -0800

    APLesson_11.1 Complete

---
+++ APLesson_11.1/GameGun.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+public class GameGun
+{
+	static int bulletCount;
+	static int CLIPSIZE = 16;
+	static int shotCount;
+	static String[] clip;
+	
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		bulletCount = 96;
+		shotCount = 0;
+		clip = new String[CLIPSIZE];
+		
+		resetClip();
+		
+		while(bulletCount > 0 || shotCount > 0)
+		{
+			System.out.println("Action: ");
+			String action = kb.nextLine();
+			if(action.equals("R"))
+			{
+				reload();
+			}
+			if(action.equals("S"))
+			{
+				System.out.println(shoot());
+			}
+			printClip();
+		}
+		System.out.println("Out of Bullets!!");
+	}
+	public static void resetClip()
+	{
+		for(int i = 0; i < clip.length; i++)
+		{
+			clip[i] = "[]";
+		}
+	}
+	public static String shoot()
+	{
+		if(shotCount > 0)
+		{
+			clip[shotCount - 1] = "[]";
+			shotCount--;
+			return "BOOM!!!";
+		}
+		else
+		{
+			return "RELOAD!!!";
+		}
+	}
+	public static void reload()
+	{
+		if(bulletCount >= CLIPSIZE)
+		{
+			bulletCount -= CLIPSIZE;
+			shotCount = CLIPSIZE;
+		}
+		else
+		{
+			shotCount = bulletCount;
+			bulletCount = 0;
+		}
+		resetClip();
+		for(int i = 0; i < shotCount; i++)
+		{
+			clip[i] = " * ";
+		}
+	}
+	public static void printClip()
+	{
+		String variable = "Bullets: " + "\t" + bulletCount + "\n" + "Clip: " + "\t";
+		for(int i = 0; i < CLIPSIZE; i++)
+		{
+			variable = variable + clip[i];
+		}
+		System.out.println(variable);
+	}
+}
